532a958ca353ff9a9448ea20acf781f15f91960e
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-12 17:36:46 +0530

    Create max.cpp

---
+++ max.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int arr[5] = {1,5,6,2,4};
+    int max = 0;
+    for(int i=0; i<5; i++)
+    {
+        if(a[i]>max) max = a[i];
+    }
+    cout<<max;
+	
+	return 0;
+}
